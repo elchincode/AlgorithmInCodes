a37e2ffd505870454ccfc7f3be48b96a07ea80f4
elchincode <elchinai@code.edu.az>
2023-01-13 12:36:55 +0400

    HomeWork

---
+++ HomeWork/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //////////////////////111111111111111111111
+            //int num = 7;
+            //int remiander = 0;
+            //int total = 0;
+
+            //while (num != 0)
+            //{
+            //    remiander = num % 10;
+            //    total += remiander;
+            //    num /= 10;
+
+
+            //}
+            //Console.WriteLine(total);
+            //if (total % 2 == 0)
+            //{
+            //    Console.Write("ededlerin cemi cutdur");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ededlerin cemi tekdir");
+            //}
+
+
+            ///////////////////////////////////22222222222222222222222
+            //int num = 100;
+            //int total = 0;
+
+
+            //while (num >= 0)
+
+            //{
+
+            //    if (num % 13 == 0)
+            //    {
+            //        Console.Write(num + ",");
+            //        total += num;
+
+
+
+
+            //    }
+            //    num--;
+
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine($"Sum of numbers divisible by 13 without a remainder is : {total} !");
+
+            ///////////////////////////////3333333333333333333333
+            //int number = 3921476;
+            //int remiander = 0;
+            //int smallest = 9;
+
+
+
+            //while (0 < number)
+            //{
+            //    remiander = number % 10;
+            //    if (smallest > remiander)
+            //    {
+
+            //        smallest = remiander;
+
+
+            //    }
+
+            //    number /= 10;
+
+
+
+            //}
+            //Console.WriteLine(smallest);
+
+
+
+
+        }
+    }
+
+}
